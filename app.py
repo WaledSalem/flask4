a1d909d0a8180812fd0877180f4a13befef5afb0
WaledSalem <75454248+WaledSalem@users.noreply.github.com>
2021-01-31 18:28:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://waled:root@localhost/data"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
